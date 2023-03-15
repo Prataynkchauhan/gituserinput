e1488b3a6fbaa0360295b6a9be02bb9a191cd75c
Pratyank Chauhan <Pratyank1996@gmail.com>
2023-03-15 15:41:29 +0530

    Update add.py

--- Input/add.py
+++ Input/add.py
@@ -1,9 +1,3 @@
-# Store input numbers
-num1 = input('Enter first number: ')
-num2 = input('Enter second number: ')
-
-# Add two numbers
-sum = float(num1) + float(num2)
-
-# Display the sum
-print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
+import os
+add = os.environ['input_value']
+print("Attaching Value1 and Value2 = ", add)
